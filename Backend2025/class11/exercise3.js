3a64d97cea9153a7d6a37fb63920b784f250c758
Fran Borges <franiart27@gmail.com>
2025-08-25 12:52:37 -0500

    add exercise3.js/class11

---
+++ Backend2025/class11/exercise3.js
@@ -0,0 +1,20 @@
+/*
+Ejercicio 3: ¿Sabes contar?
+Consigna:
+Crea un programa que le pida al usuario un número positivo. El programa
+deberá contar desde el 1 hasta ese número e imprimir cada valor en la consola.
+Usa un ciclo for para realizar la tarea.
+*/
+
+const prompt = require('prompt-sync')();
+
+let num = parseInt(prompt("Ingrese un número positivo:"));
+
+if (num > 0) {
+    for (let i=1; i <= num; i++) {
+        console.log(`Contando: ${i}`);
+    }
+} else {
+    console.log("Por favor, ingrese un número positivo.");  
+}
+
