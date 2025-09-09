dfd14dd6fdad7cf1e5653ae493a5a5a174dd1163
Fran Borges <franiart27@gmail.com>
2025-09-09 13:41:48 -0500

    add exercise10.js/class15

---
+++ Backend2025/class15/exercise10.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 10: Reemplazar una palabra en una frase
+Tienes la frase "Me gusta el invierno". Cambia la palabra "invierno" por
+"verano" y muestra la frase actualizada.
+*/
+
+let originalPhrase = "Me gusta el invierno";
+let updatedPhrase = originalPhrase.replace("invierno", "verano");
+console.log(updatedPhrase);
