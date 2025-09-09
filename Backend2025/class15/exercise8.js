72b4fd03a14a9572047642bfa8758627e95b9b3a
Fran Borges <franiart27@gmail.com>
2025-09-09 13:36:38 -0500

    add exercise8.js/class15

---
+++ Backend2025/class15/exercise8.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 8: Extraer una parte de una frase
+Tienes la frase "Los gatos son geniales". Extrae y muestra solo la parte
+"gatos" utilizando el método adecuado.
+*/
+
+let phrase = "Los gatos son geniales";
+let extractedPart = phrase.slice(4, 9);
+console.log(extractedPart);
