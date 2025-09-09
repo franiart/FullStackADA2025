13c7555b787163b76fdfd9bfb90998d763dbb7ed
Fran Borges <franiart27@gmail.com>
2025-09-09 13:26:58 -0500

    add exercise5.js/class15

---
+++ Backend2025/class15/exercise5.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 5: Buscar la posición de un elemento
+Tienes una lista de colores ["Rojo", "Verde", "Azul", "Amarillo"]. Encuentra y
+muestra el índice del color "Azul" en la lista.
+*/
+
+let colors = ["Rojo", "Verde", "Azul", "Amarillo"];
+let indexOfBlue = colors.indexOf("Azul");
+console.log(`El índice del color "Azul" es: ${indexOfBlue}`);
