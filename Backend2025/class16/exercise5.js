f92d199f6f1fe037a4fed9e18594dc7d7c73df4a
Fran Borges <franiart27@gmail.com>
2025-09-12 11:08:27 -0500

    add exercise5.js/class16

---
+++ Backend2025/class16/exercise5.js
@@ -0,0 +1,8 @@
+/*
+Ejercicio 5: Recorre una lista e imprime cada elemento
+Tienes una lista de frutas ['manzana', 'banana', 'cereza']. Tu tarea es recorrer
+la lista y mostrar el nombre de cada fruta en la consola, una por una.
+*/
+
+let fruits = ['manzana', 'banana', 'cereza'];
+fruits.forEach(fruit => console.log(fruit));
